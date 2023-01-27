d172fda977c322cd333f8e93726eaf77d9a1f45f
larrbp <richmond.lau@bp.com>
2023-01-27 19:27:40 +0800

    Added error messages to RuntimeErrors thrown when asserting results

--- Util/AnswerChecker.py
+++ Util/AnswerChecker.py
@@ -20,15 +20,17 @@ class AnswerChecker:
             self.logger.error(f"FAILED:\nExpected:\t{expected}\nActual:\t\t{actual}")
             if expected is not None and actual is not None:
                 if len(expected) != len(actual):
-                    self.logger.error(f"Expected a string of length {len(expected)} but received a string of length "
-                                      f"{len(actual)}")
-                    raise RuntimeError()
+                    e1 = f"Expected a string of length {len(expected)} but received a string of length " \
+                         f"{len(actual)}"
+                    self.logger.error(e1)
+                    raise RuntimeError(e1)
                 for i in range(len(expected)):
                     if expected[i] != actual[i]:
-                        self.logger.error(f"First difference occurred at position {i}: "
-                                          f"{expected[max(0, i-2):i]}>>>{expected[i]}<<<"
-                                          f"{expected[min(len(expected)-1, i+1):min(len(expected)-1, i+2)+1]}")
-                        raise RuntimeError()
+                        e2 = f"First difference occurred at position {i}: " \
+                             f"{expected[max(0, i-2):i]}>>>{expected[i]}<<<" \
+                             f"{expected[min(len(expected)-1, i+1):min(len(expected)-1, i+2)+1]}"
+                        self.logger.error(e2)
+                        raise RuntimeError(e2)
 
     def __get_puzzle_input(self, input_file_path):
         self.logger.info(f"Reading file {input_file_path} to get puzzle input")
