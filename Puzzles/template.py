2a1cb9ea654240472064bc9be58b7a4c7a3337f9
larrbp <richmond.lau@bp.com>
2023-01-28 14:37:06 +0800

    Added method in AnswerChecker.py to automatically generate files for the next day and updated template file

--- Puzzles/template.py
+++ Puzzles/template.py
@@ -11,9 +11,11 @@ def answer(a):
 
 if __name__ == "__main__":
     test_input = """"""
-    expected_output = """"""
+    expected_output = ""
 
     answer_checker = AnswerChecker()
     answer_checker.check(expected_output, answer(test_input))
 
     answer_checker.generate_puzzle_output(__file__, answer)
+
+    # answer_checker.generate_next_day_files(__file__)
--- Util/AnswerChecker.py
+++ Util/AnswerChecker.py
@@ -37,13 +37,13 @@ class AnswerChecker:
         with open(Path(input_file_path)) as f:
             return f.read()
 
-    def __ensure_directory_exists(self, directory):
+    def __create_directory_if_not_exists(self, directory):
         if not os.path.exists(directory):
             self.logger.info(f"Creating directory {directory}")
             os.mkdir(directory)
 
     def __write_results_to_file(self, results, file_name, directory):
-        self.__ensure_directory_exists(directory)
+        self.__create_directory_if_not_exists(directory)
         file_path = os.path.join(directory, file_name.replace(".py", ".txt"))
         self.logger.info(f"Writing results to file {file_path}")
         with open(file_path, "w") as f:
@@ -58,3 +58,27 @@ class AnswerChecker:
         output_text = function(input_text)
         self.__write_results_to_file(output_text, file_name, output_path)
         self.logger.info(f"Answer: {output_text}")
+
+    def generate_next_day_files(self, puzzle_file_path):
+        day_path, file_name = os.path.split(os.path.abspath(puzzle_file_path))
+        puzzle_path, day = os.path.split(os.path.abspath(day_path))
+        next_day = "Day" + str(int(day.split("Day")[1]) + 1).rjust(2, "0")
+
+        with open(os.path.join(puzzle_path, "template.py"), "r") as f:
+            template_content = f.read()
+            new_day_path = os.path.join(puzzle_path, next_day)
+            self.__create_directory_if_not_exists(new_day_path)
+            with open(os.path.join(new_day_path, next_day + "_01.py"), "w") as p1:
+                p1.write(template_content)
+            with open(os.path.join(new_day_path, next_day + "_02.py"), "w") as p2:
+                p2.write(template_content)
+
+        input_path = os.path.join(os.path.join(Path(puzzle_path).parent, "Inputs"), next_day)
+        self.__create_directory_if_not_exists(input_path)
+        f2 = open(os.path.join(input_path, next_day + ".txt"), "w")
+        f2.close()
+
+
+
+
+
